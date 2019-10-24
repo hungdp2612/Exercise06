6ffffca82e737d882f9c896f611279315379a707
hung2612 <hungdp2612@gmail.com>
2019-10-24 17:42:44 +0700

    Stack

---
+++ Stack/UnitTestStack/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem2;
+
+namespace UnitTestStack
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPush()
+        {
+            Stack S = new Stack(1000);
+            S.Push(1);
+            S.Push(2);
+            S.Push(3);
+            Assert.AreEqual(3, S.Peek());
+            Assert.AreEqual(3, S.Pop());
+        }
+    }
+}
